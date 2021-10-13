fa7bb2540cd01e3daaa8637cdb24a1fcdf6229a5
chrkrogh <75618575+chrkrogh@users.noreply.github.com>
2021-10-13 11:43:08 +0200

    Update CallKinDrape.py
    Added "ax" and "fig" as output variables from KinDrape

--- Python implementation/CallKinDrape.py
+++ Python implementation/CallKinDrape.py
@@ -18,5 +18,5 @@ d = 0.075; Grid = [24,24]; Org = [-d/2, -d/2]; Ang = 0; OrgNode = [11,11]
 
 # Time and call KinDrape
 tic = time.time()
-Node, Shear = kd.KinDrape(d, Grid, Org, Ang, OrgNode) 
+Node, Shear, ax, fig = kd.KinDrape(d, Grid, Org, Ang, OrgNode) 
 print('Time spent on drape analysis: ', time.time() - tic, ' s')
